144654fc34bc1d4c2d583fd80ddad87f226d164f
Ni1py <gekepyrkov@gmail.com>
2021-05-16 20:57:37 +0300

    сделал header и footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { LoginButtonComponent } from './Components/header/login-button/login-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginButtonComponent
   ],
   imports: [
     BrowserModule,
