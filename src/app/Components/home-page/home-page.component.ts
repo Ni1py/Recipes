fcbb1b0088ebfc822ae22a1a36ba9ff73402585e
Ni1py <gekepyrkov@gmail.com>
2021-05-24 19:57:36 +0300

    Переместил инициализацию из home-page в recipe-of-the-day

--- src/app/Components/home-page/home-page.component.ts
+++ src/app/Components/home-page/home-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CardContent } from '../home-page/recipe-of-the-day/recipe-of-the-day-card';
 
 @Component({
   selector: 'app-home-page',
@@ -7,20 +6,11 @@ import { CardContent } from '../home-page/recipe-of-the-day/recipe-of-the-day-ca
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  public card!: CardContent;
+
   constructor() { 
   }
 
   ngOnInit(): void {
-    this.card =
-    {
-      image: "../../../../assets/images/day-recipe.png",
-      likes: "356",
-      time: "35 минут",
-      title: "Тыквенный супчик на кокосовом молоке",
-      text: "Если у вас осталась тыква, и вы не знаете что с ней сделать, то это решение для вас! Ароматный, согревающий суп-пюре на кокосовом молоке. Можно даже в Пост!",
-      author: "@glazest",
-    }
   }
 
 }
--- src/app/Components/home-page/recipe-of-the-day/recipe-of-the-day.component.ts
+++ src/app/Components/home-page/recipe-of-the-day/recipe-of-the-day.component.ts
@@ -7,13 +7,20 @@ import { CardContent } from '../recipe-of-the-day/recipe-of-the-day-card';
   styleUrls: ['./recipe-of-the-day.component.css']
 })
 export class RecipeOfTheDayComponent implements OnInit {
-
-  @Input() card!: CardContent;
-
-  constructor() { }
+  public card!: CardContent;
+  constructor() { 
+  }
 
   ngOnInit(): void {
-
+    this.card =
+    {
+      image: "../../../../assets/images/day-recipe.png",
+      likes: "356",
+      time: "35 минут",
+      title: "Тыквенный супчик на кокосовом молоке",
+      text: "Если у вас осталась тыква, и вы не знаете что с ней сделать, то это решение для вас! Ароматный, согревающий суп-пюре на кокосовом молоке. Можно даже в Пост!",
+      author: "@glazest",
+    }
   }
 
 }
